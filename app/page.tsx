8ecdf000b79709014334c245af9efc7756e1d8ff
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-10-17 18:42:13 +0300

    Update layout and styles for portfolio presentation; add animations and new images

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,114 @@
-import Image from "next/image";
-
+import { Mail, Linkedin, Github, Dribbble } from 'lucide-react';
+import Image from 'next/image';
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main>
+      <section className="bg-sky-100 ">
+        <div className='grid md:grid-cols-2 sm:grid-cols-1'>
+          <div>
+            <Image src="/AiME.png" alt="" height={1500} width={1700} className="overflow-hidden"/> 
+          </div>
+          <div className=' text-center flex flex-col justify-center items-center '>
+            <h1 className='text-pink-500 text-8xl'>port <br /> <span>folio</span></h1>
+            <h3 className='text-2xl text-black'>zeamanuel fetene <span>|</span> ui/ux designer</h3>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section className='bg-sky-100 text-black p-20 '>
+        <div className='grid md:grid-cols-2 sm:grid-cols-1 gap-10'>
+          <div>
+            <h1 className='text-9xl text-pink-500 text-center'>Hello <span>!</span></h1>
+            <hr className='h-2 w-170' />
+            <p>
+              I'm Zeamanuel Fetene, a passionate UI/UX designer dedicated to crafting exceptional digital experiences. With a keen eye for detail and a user-centric approach, I specialize in creating intuitive and visually appealing designs that resonate with users. 
+              <br />  My portfolio showcases a diverse range of projects, highlighting my ability to blend creativity with functionality. Explore my work to see how I bring ideas to life through innovative design solutions.
+            </p>
+            <div >
+             <h3 className='flex flex-cols-2'> <Mail  className='text-pink-500 pr-2 fill-pink-200/50' size={35}/>zeamanuel@gmail.com</h3>
+              <h3 className='flex flex-cols-2'> <Linkedin className='text-pink-500 pr-2 fill-pink-200' size={35}/> linkedin.com/in/zeamanuel</h3>
+              <h3 className='flex flex-cols-2'> <Github className='text-pink-500 pr-2 fill-pink-200' size={35}/> github.com/zeamanuel</h3> 
+            </div>
+
+          </div>
+          {/* second section image */}
+           <div>
+              <Image
+          src="/sece.png"
+          alt="Secondary illustration"
+          height={212}
+          width={308}
+          className="overflow-hidden"/>
+
+  </div>
+        </div>
+      </section>
+      
+      <section className="bg-sky-50 py-20">
+  <div className="flex flex-col md:flex-row items-center justify-center gap-10">
+    
+    {/* Left: Image */}
+    <div className="flex justify-center items-center p-10">
+  <div className="animate-spinY">
+    <Image
+      src="/sece.png"
+      alt="Secondary illustration"
+      height={212}
+      width={308}
+      className="overflow-hidden rounded-lg"
+    />
+  </div>
+</div>
+
+
+    {/* Vertical Divider */}
+    <div className="w-[1px] h-100 bg-gray-400 mx-4"></div>
+
+    {/* Right: Experience / Education / Skills */}
+    <div className="text-black">
+      <div>
+      <h2 className="font-semibold mb-2 text-6xl text-pink-500">Experience</h2>
+      <ul className="grid md:grid-cols-2 gap-6 text-sm ">
+        <li>2018 - UI/UX Designer at XYZ Company</li>
+        <li>2020 - Senior Designer at ABC Agency</li>
+        <li>2022 - Lead Designer at DEF Corp</li>
+        <li>2023 - Principal Designer at GHI Inc</li>
+      </ul>
+      </div>
+      {/* Education */}
+      <div>
+      <h2 className="font-semibold mb-2 mt-4 text-6xl text-pink-500">Education</h2>
+      <ul className=" gap-2 text-sm">
+        <li>B.A. in Graphic Design, University of Art (2014-2018)</li>
+        <li>M.S. in Human-Computer Interaction, Tech University (2018-2020)</li>
+      </ul>
+
+      </div>
+
+      {/* Skills */}
+      <div>
+      <h2 className="font-semibold mb-2 mt-4 text-6xl text-pink-500">Skills</h2>
+      <ul className="grid md:grid-cols-2 gap-2 text-sm">
+        <li>Wireframing & Prototyping</li>
+        <li>User Research & Testing</li>
+        <li>Visual Design</li>
+        <li>Interaction Design</li>
+        <li>Figma & Sketch</li>
+        <li>Adobe Creative Suite</li>
+      </ul>
+      </div>
     </div>
+
+  </div>
+
+      </section>
+
+      {/* The projects */}
+
+      <section>
+        
+      </section>
+
+    </main>
   );
 }
